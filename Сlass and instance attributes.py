4df7a2d535981fafe082ca5a8f83617905e939d9
Alexey Kuzminykh <114499756+Al3x3y86@users.noreply.github.com>
2024-05-21 20:52:28 +0300

    Различие атрибутов класса и экземпляра

--- Сlass and instance attributes.py
+++ Сlass and instance attributes.py
@@ -1,7 +1,7 @@
 # Ваша задача:
 # Создайте новый класс Buiding с атрибутом total
 # Создайте инициализатор для класса Buiding, который будет увеличивать атрибут количества созданных объектов класса
-# Building total 
+# Building total
 # В цикле создайте 40 объектов класса Building и выведите их на экран командой print
 
 class Building:
@@ -9,16 +9,11 @@ class Building:
 
     def __init__(self):
         Building.total += 1
-        print(self)  # Выводим созданный объект при инициализации
 
-    def __str__(self):
-        return f'Building {Building.total}'  # Переопределяем строковое представление объекта
-
-
+# Создаем 40 объектов класса Building
 for _ in range(40):
     b = Building()
-
-print(f'Total buildings created: {Building.total}')
+    print(f'Building {Building.total}')  # Выводим созданный объект на экран
 
 
 
